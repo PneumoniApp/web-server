aadacc657b47e5b9d8d4cd04d68379a5dab02479
eliseooortiz <eliseo271099@gmail.com>
2022-09-25 20:03:35 +0000

    changes added to server, sql file modified because of incompatible collate

--- prediction_xraybck_table.sql
+++ prediction_xraybck_table.sql
@@ -26,7 +26,7 @@ CREATE TABLE `prediction_xraybck` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `img` longblob NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=utf8mb4 ;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
