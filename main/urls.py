a4e5d377169f74830cab4750b8d7e7b1b317fbcb
eliseooortiz <eliseo271099@gmail.com>
2022-09-29 00:26:14 +0000

    Server issue fixed and minor changes to views

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-path("<int:id>",views.index, name="index"),
 path("",views.initial,name="initial"),
 path("home/",views.home,name="home"),
 path("create/",views.create,name="create"),
