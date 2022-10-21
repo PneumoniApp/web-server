5c56ecd98cd5b53b68a1a5ec0667d051a628d19c
Eliseo ortiz <eliseo271099@gmail.com>
2022-10-21 14:44:48 -0500

    changing urls

--- main/urls.py
+++ main/urls.py
@@ -12,4 +12,5 @@ path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_n
 path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/password/password_reset_confirm.html"), name='password_reset_confirm'),
 path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='main/password/password_reset_complete.html'), name='password_reset_complete'),
 path('login/', auth_views.LoginView.as_view(), name='login'),
+path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
